68121667e166174ea72df73a80ef1235aabb180d
Bhasker Kamshetty <bhaskern.kamshetty@gmail.com>
2025-01-26 00:24:02 +0530

    created hello.js

---
+++ cypress/java-script/hello.js
@@ -0,0 +1,4 @@
+console.log("HELLO"); // To display Text or String
+console.log("WORLD"); // To display Text or String
+console.log(10); // To display an Integer
+console.log(10.5); // To display an Float
